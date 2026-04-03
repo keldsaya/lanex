6bdb434b98643f1ad2b6ba938866c665f2f99db7
keldsaya <keldsaya@gmail.com>
2026-04-03 17:51:13 +0800

    updated tty.c

--- src/tty.c
+++ src/tty.c
@@ -28,6 +28,10 @@ void terminal_initialize(void) {
 
 void terminal_setcolor(uint8_t color) { terminal_color = color; }
 
+void terminal_scroll() {
+  /* TODO: make terminal scrolling */
+}
+
 void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) {
   const size_t index = y * VGA_WIDTH + x;
   terminal_buffer[index] = vga_entry(c, color);
