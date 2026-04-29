3d1d23e8d4a8b89cc2e290ffb55de6df912c81c0
keldsaya <keldsaya@gmail.com>
2026-04-29 23:50:03 +0800

    add mem-() in string.h

---
+++ libc/src/memcmp.c
@@ -0,0 +1,13 @@
+#include <string.h>
+
+int memcmp(const void* ptr1, const void* ptr2, size_t num) {
+  const unsigned char* p1 = ptr1;
+  const unsigned char* p2 = ptr2;
+  while (num--) {
+    if (*p1 != *p2)
+      return *p1 - *p2;
+    p1++;
+    p2++;
+  }
+  return 0;
+}
